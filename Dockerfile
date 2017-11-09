a943d294176e4f14d1a14495bf3d64e71e3bed86
Andreas Wachowski <andreas.wachowski@gmail.com>
2017-11-09 17:25:01 +0100

    Remove temporary files, create extensions

--- Dockerfile
+++ Dockerfile
@@ -18,14 +18,22 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get update -qq \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
     && truncate -s 0 /var/log/*log
 
-RUN wget https://github.com/omniti-labs/pg_jobmon/archive/v1.3.3.zip \
+RUN cd /tmp \
+    && wget https://github.com/omniti-labs/pg_jobmon/archive/v1.3.3.zip \
     && unzip v1.3.3.zip \
     && make -C pg_jobmon-1.3.3 \
-    && make NO_BGW=1 install -C pg_jobmon-1.3.3
+    && make NO_BGW=1 install -C pg_jobmon-1.3.3 \
+    && rm v1.3.3.zip \
+    && rm -rf pg_jobmon-1.3.3
 
-RUN wget https://github.com/keithf4/pg_partman/archive/v3.1.0.zip \
+RUN cd /tmp \
+    && wget https://github.com/keithf4/pg_partman/archive/v3.1.0.zip \
     && unzip v3.1.0.zip \
     && make -C pg_partman-3.1.0 \
-    && make install -C pg_partman-3.1.0
+    && make install -C pg_partman-3.1.0 \
+    && rm v3.1.0.zip \
+    && rm -rf pg_partman-3.1.0
 
 ENV PATH /usr/bin:$PATH
+
+COPY initdb.sh /docker-entrypoint-initdb.d/
---
+++ initdb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+psql -v ON_ERROR_STOP=1 --username postgres template1 <<-EOSQL
+CREATE EXTENSION hstore;
+CREATE EXTENSION pg_trgm;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+CREATE EXTENSION hstore;
+CREATE EXTENSION pg_trgm;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE SCHEMA dblink;
+    CREATE EXTENSION dblink SCHEMA dblink;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE SCHEMA jobmon;
+    CREATE EXTENSION pg_jobmon SCHEMA jobmon;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE SCHEMA partman;
+    CREATE EXTENSION pg_partman SCHEMA partman;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT USAGE ON SCHEMA jobmon TO $POSTGRES_USER;
+    GRANT USAGE ON SCHEMA dblink to $POSTGRES_USER;
+    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA jobmon TO $POSTGRES_USER;
+    GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA jobmon TO $POSTGRES_USER;
+    GRANT ALL ON ALL SEQUENCES IN SCHEMA jobmon TO $POSTGRES_USER;
+EOSQL
